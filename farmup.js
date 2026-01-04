1c6992137870f34f834999a2f91b37a1f338bc03
tsibissolution <71134156+tsibissolution@users.noreply.github.com>
2026-01-04 22:09:46 +0530

    Update farmup.js

--- farmup.js
+++ farmup.js
@@ -24,16 +24,16 @@ const api = axios.create({
 const plantSeed = [
   {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68c7b8915c457c186fd40041","seedIDs": "665f2698534176fcd32f9a7d","growthTime": 1800000},
   {"userGardensIDs": "68c2eb6a7e204da0fe55f668","userBedsIDs": "68dd8aa03bdf0c6e7893f8ed","seedIDs": "665f2698534176fcd32f9a7d","growthTime": 1800000},  
-  {"userGardensID":"68c2eb6a7e204da0fe55f668","userBedsID":"692c56ded5279610094c3213","seedID":"68824771915623f3dcc1fb09", "growthTime":21600000},
-{"userGardensID":"68c2eb6a7e204da0fe55f668","userBedsID":"692c575064e8f74db4ad0094","seedID":"673e0c942c7bfd708b352489", "growthTime":6720000},
-{"userGardensID":"68c2eb6a7e204da0fe55f668","userBedsID":"692c571fd5279610094c548f","seedID":"67dc227a59b878f195998db8", "growthTime":1920000},
-{"userGardensID":"68c2eb6a7e204da0fe55f668","userBedsID":"692c56f964e8f74db4acd14f","seedID":"67dc227a59b878f195998d7c", "growthTime":18303000},
-{"userGardensID":"68c2eb6a7e204da0fe55f668","userBedsID":"690b47dd284c0906f5ae679d","seedID":"67dc227a59b878f195998dd6", "growthTime": 900000},
-{"userGardensID":"68c2eb6a7e204da0fe55f668","userBedsID":"695a826c64968779250095a2","seedID":"673e0c942c7bfd708b35244d", "growthTime": 120000},
-{"userGardensID":"68c2eb6a7e204da0fe55f668","userBedsID":"694b90631541358fb2678528","seedID":"67dc227a59b878f195998ea2", "growthTime": 9360000},
-{"userGardensID":"68c2eb6a7e204da0fe55f668","userBedsID":"692ea0a0337dbc729f96b95b","seedID":"67dc227a59b878f195998e60", "growthTime": 28140000},
-{"userGardensID":"68c2eb6a7e204da0fe55f668","userBedsID":"695a83e164968779250129e3","seedID":"673e0c942c7bfd708b352405", "growthTime": 720000},
-{"userGardensID":"68c2eb6a7e204da0fe55f668","userBedsID":"695a17456496877925db9108","seedID":"673e0c942c7bfd708b352465", "growthTime": 240000},
+  {"userGardensIDs":"68c2eb6a7e204da0fe55f668","userBedsIDs":"692c56ded5279610094c3213","seedIDs":"68824771915623f3dcc1fb09", "growthTime":21600000},
+{"userGardensIDs":"68c2eb6a7e204da0fe55f668","userBedsIDs":"692c575064e8f74db4ad0094","seedIDs":"673e0c942c7bfd708b352489", "growthTime":6720000},
+{"userGardensIDs":"68c2eb6a7e204da0fe55f668","userBedsIDs":"692c571fd5279610094c548f","seedIDs":"67dc227a59b878f195998db8", "growthTime":1920000},
+{"userGardensIDs":"68c2eb6a7e204da0fe55f668","userBedsIDs":"692c56f964e8f74db4acd14f","seedIDs":"67dc227a59b878f195998d7c", "growthTime":18303000},
+{"userGardensIDs":"68c2eb6a7e204da0fe55f668","userBedsIDs":"690b47dd284c0906f5ae679d","seedIDs":"67dc227a59b878f195998dd6", "growthTime": 900000},
+{"userGardensIDs":"68c2eb6a7e204da0fe55f668","userBedsIDs":"695a826c64968779250095a2","seedIDs":"673e0c942c7bfd708b35244d", "growthTime": 120000},
+{"userGardensIDs":"68c2eb6a7e204da0fe55f668","userBedsIDs":"694b90631541358fb2678528","seedIDs":"67dc227a59b878f195998ea2", "growthTime": 9360000},
+{"userGardensIDs":"68c2eb6a7e204da0fe55f668","userBedsIDs":"692ea0a0337dbc729f96b95b","seedIDs":"67dc227a59b878f195998e60", "growthTime": 28140000},
+{"userGardensIDs":"68c2eb6a7e204da0fe55f668","userBedsIDs":"695a83e164968779250129e3","seedIDs":"673e0c942c7bfd708b352405", "growthTime": 720000},
+{"userGardensIDs":"68c2eb6a7e204da0fe55f668","userBedsIDs":"695a17456496877925db9108","seedIDs":"673e0c942c7bfd708b352465", "growthTime": 240000},
 ];
 
 
@@ -170,6 +170,7 @@ async function startFarm() {
 }
 
 startFarm().catch(err => console.error("💥 Fatal error:", err.message));
+
 
 
 
